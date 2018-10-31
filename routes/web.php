e541ccf36d1882ab767c6bb2c4bd36f6582fc8c1
Mutiara Windi P <mutiarawindi@gmail.com>
2018-10-31 12:50:05 +0700

    add template

--- routes/web.php
+++ routes/web.php
@@ -23,5 +23,14 @@ Route::get('pesan', function () { //sesuai a href
 Route::post('pesan/kirim', function () { //sesuai a href
     return view('kirim');	//sesuai nama file di view
 });
+Route::get('template', function () { //sesuai a href
+    return view('layouts.master');	//sesuai nama file di view layouts = nama folder, master = nama file, dipisahkan dengan tanda titik
+});
+Route::get('coba1', function () { //sesuai a href
+    return view('coba1');	//sesuai nama file di view
+});
+Route::get('coba2', function () { //sesuai a href
+    return view('coba2');	//sesuai nama file di view
+});
 
 
